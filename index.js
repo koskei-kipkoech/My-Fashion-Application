ec2f7653a126bc3bc6d7fd74d29b671ac3d0ca76
Patrick Kipkoech <patrickwayy@gmail.com>
2024-10-16 17:44:51 +0300

    updates

--- index.js
+++ index.js
@@ -15,3 +15,39 @@ saveBar.addEventListener('click', () => {
 menuBar.addEventListener('click', () =>{
     myNav.classList.toggle('active')
 })
+
+//swiper code for the slide gallery
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 40,
+
+    //autoplay setting
+    autoplay:{
+        delay:2000,
+        disableOnInteraction: false,
+    },
+
+    // page bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
