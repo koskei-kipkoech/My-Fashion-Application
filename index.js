0b65ddc87494c5c0240058a22628a3823237c602
Patrick Kipkoech <patrickwayy@gmail.com>
2024-10-18 07:58:12 +0300

    adds event listeners to products

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ let saveBar = document.querySelector('#save-bar');
 let shopCart = document.querySelector('.shopping-cart');
 let menuBar = document.querySelector('#menu-bar');
 let myNav = document.querySelector('.navbar');
+let body = document.querySelector('body')
+let closeCart = document.querySelector('.close')
+let listProductHTML = document.querySelector('.main-product')
+let listCartHTML = document.querySelector('.shopping-box')
+let iconSpanTab = document.querySelector('#count')
+let listProducts = []
+let carts = []
 
 searchBar.addEventListener('click', () => {
     console.log('here clickkeed')
     searchBox.classList.toggle('active');
 })
 saveBar.addEventListener('click', () => {
-    shopCart.classList.toggle('active')
+    body.classList.toggle('showCart')
 })
 menuBar.addEventListener('click', () =>{
     myNav.classList.toggle('active')
+})
+closeCart.addEventListener('click',() =>{
+    body.classList.toggle('showCart')
 })
 
 //swiper code for the slide gallery
@@ -51,3 +61,154 @@ new Swiper('.card-wrapper', {
         },
     }
 });
+
+const addDataToHTML = () =>{
+    listProductHTML.innerHTML = ''
+    if(listProducts.length > 0){
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div')
+            newProduct.classList.add('inner-product')
+            //newProduct.dataset.id = product_id; 
+            newProduct.dataset.id = product.id;
+            newProduct.innerHTML = `
+            <img src="${product.images}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">$${product.price}</div>
+                <button class="addCart">add favourite</button>
+            `
+            listProductHTML.appendChild(newProduct)
+        });
+    }
+}
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')){
+        let product_id = positionClick.parentElement.dataset.id; 
+        addToCart(product_id)
+    }
+})
+const addToCart = (product_id) => {
+    let positionInCart = carts.findIndex((value) => value.product_id == product_id);
+    if(carts.length <= 0){
+        carts = [{
+            product_id:product_id,
+            quantity:1
+        }]
+        alert("Product added to favourite")
+    }else if(positionInCart < 0){
+        carts.push({
+            product_id:product_id,
+            quantity:1
+        })
+    }else{
+        carts[positionInCart].quantity = carts[positionInCart].quantity + 1
+
+    }
+    addCartToHTML()
+    addCartToMemory()
+}
+const addCartToMemory = () => {
+    localStorage.setItem('cart',JSON.stringify(carts))
+}
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    //console.log('positionclicked', positionClick)
+
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+        let qualityDiv = positionClick.closest('.quantity')
+        let product_id = qualityDiv.dataset.id;
+       // console.log('product id', product_id)
+
+        let type = 'minus'
+        if(positionClick.classList.contains('plus')){
+            type = 'plus'
+        }
+        changeQuantity(product_id,type);
+    }
+})
+const changeQuantity = (product_id, type) => {
+    let positionOnItemInCart = carts.findIndex((value) => value.product_id == product_id);
+    console.log('current cart',carts)
+    console.log('chabging quality', product_id, 'type',type)
+    if(positionOnItemInCart >= 0){
+        switch(type){
+            case 'plus':
+                carts[positionOnItemInCart].quantity = carts[positionOnItemInCart].quantity + 1;
+                break
+
+            default:
+                let valueChange = carts[positionOnItemInCart].quantity - 1;
+                if( valueChange > 0){
+                    carts[positionOnItemInCart].quantity = valueChange;
+                }else{
+                    carts.splice(positionOnItemInCart,1)
+                }
+                break
+        }
+    }
+    addCartToMemory()
+    addCartToHTML()
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = ''
+    let totalQuantity = 0
+    if(carts.length > 0){
+        carts.forEach(cart => {
+            totalQuantity = totalQuantity + cart.quantity
+            let newcart = document.createElement('div');
+            newcart.classList.add('item')
+            let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id)
+            let info = listProducts[positionProduct];
+            newcart.innerHTML = `
+            <div class="image">
+                <img src="${info.images}" alt="">
+            </div>
+            <div class="name">
+                ${info.name}
+            </div>
+            <div class="totalPrice">
+                ${info.price *cart.quantity}
+            </div>
+            <div class="quantity" data-id = "${info.id}">
+                <span class="minus"><</span>
+                <span>${cart.quantity}</span>
+                <span class="plus">></span>
+            </div>
+            `;
+            listCartHTML.appendChild(newcart)
+        })
+    }
+    iconSpanTab.innerText = totalQuantity
+}
+const atTheApp = () => {
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        listProducts = data;
+        addDataToHTML()
+
+        //check the local storage for saved favourite 
+        if (localStorage.getItem('cart')){
+            carts =  JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML()
+        }else{
+            carts = []
+        }
+    })
+    .catch(error =>{
+        console.log('Error fetching',error)
+        alert('Failed to load products. Try again later')
+    })
+}
+atTheApp()
+
+
+
+
+
+
+//fetching code for items 
+
+// fetch("https://fakestoreapi.com/products")
+// .then((res) => res.json())
+// .then((json) => console.log(json));
